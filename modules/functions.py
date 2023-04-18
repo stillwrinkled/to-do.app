3bac0e09a8aa0802b6af34d884c71089dc7aa5df
amit_1 <amit@stillwrinkled.com>
2023-04-18 19:25:17 +0530

    Added a new feature. 'Add' feature

--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,7 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of
     to-do items"""
     with open(filepath, 'r') as file_local:
@@ -6,7 +9,7 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
